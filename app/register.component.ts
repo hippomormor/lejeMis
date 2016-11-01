c0cb17630752d508ff3b4115f921aa2e61498140
Hippomormor <cgenter@oncable.dk>
2016-11-01 15:02:28 +0100

    Added See contract

--- app/register.component.ts
+++ app/register.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit}  from '@angular/core';
 import {Location}           from '@angular/common';
-import {Headers, Http}    from '@angular/http';
+import {Headers, Http}      from '@angular/http';
 
 import {AppComponent}       from "./app.component"
 import {Lejer}              from "./lejer";
@@ -17,9 +17,11 @@ import 'rxjs/add/operator/toPromise';
 })
 
 export class RegisterComponent implements OnInit {
-    private lejerURL = 'http://custom-env.huc3devb74.eu-central-1.elasticbeanstalk.com/api/lejer';
+    private lejerURL = 'http://custom-env.huc3devb74.eu-central-1.elasticbeanstalk.com/api/lejekontrakt';
+    private kontraktURL = null;
     private headers = new Headers({'Content-Type': 'application/json'});
     private myLejer = new Lejer();
+    public doneButton = false;
 
     constructor(
         private appComponent : AppComponent,
@@ -38,19 +40,28 @@ export class RegisterComponent implements OnInit {
        this.create(this.myLejer);
     }
 
-    create(lejer: Lejer): Promise<Lejer> {
+    create(lejer: Lejer): Promise<void> {
         console.log('Posting to DB');
         return this.http
             .post(this.lejerURL, JSON.stringify(lejer), {headers: this.headers})
             .toPromise()
-            //.then(() => lejer)
-            .then(res => res.json().data)
+            .then(res => {
+                this.kontraktURL = this.lejerURL + "/" + res.text();
+                this.doneButton = true;
+                console.log(this.kontraktURL);
+            })
             .catch(RegisterComponent.handleError);
     }
 
     private static handleError(error: any): Promise<any> {
         console.error('An error occurred', error);
         return Promise.reject(error.message || error);
+    }
+
+    goToContract() {
+        this.doneButton = false;
+        window.open(this.kontraktURL);
+        this.kontraktURL = null;
     }
 }
 
